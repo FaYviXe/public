fb2ec71d5574b278124db267fa298cf500ae3a02
FaYviXe <54771198+FaYviXe@users.noreply.github.com>
2024-02-13 21:14:52 +0100

    Update shrek.sh

--- shrek.sh
+++ shrek.sh
@@ -1,4 +1,4 @@
-curl -o /home/$USER/Music/Shrek.mp4 "https://cdn.discordapp.com/attachments/723258310559662160/1207047699510067340/shrek.mp4-ex=65de39c5&is=65cbc4c5&hm=e9544b5ef72c3812f31054b44917fd5fb76ec96019e97cd60ce8fbd593551952&"
+curl -o /home/$USER/Music/Shrek.mp4 "https://cdn.discordapp.com/attachments/723258310559662160/1207047699510067340/shrek.mp4?ex=65de39c5&is=65cbc4c5&hm=e9544b5ef72c3812f31054b44917fd5fb76ec96019e97cd60ce8fbd593551952&"
 
 pkill ft_lock; ft_lock
 
@@ -9,7 +9,7 @@ export LD_LIBRARY_PATH="/sgoinfre/goinfre/Perso/aboyreau/usr/lib/x86_64-linux-gn
 vlc --video-on-top --no-video-deco --play-and-exit --fullscreen --preferred-resolution 1080 ~/Music/Shrek.mp4 & 
 sleep 0.3
 
-window_id=`xwininfo -name "shrek.mp4 - VLC media player" | awk '/Window id:/{print $4}'`
+window_id=`xwininfo -name "Shrek.mp4 - VLC media player" | awk '/Window id:/{print $4}'`
 
 /sgoinfre/goinfre/Perso/aboyreau/usr/bin/xdotool set_window --overrideredirect 1 $window_id
 /sgoinfre/goinfre/Perso/aboyreau/usr/bin/xdotool windowunmap --sync $window_id
@@ -19,4 +19,5 @@ window_id=`xwininfo -name "shrek.mp4 - VLC media player" | awk '/Window id:/{pri
 while xwininfo -name ft_lock >/dev/null 2>&1; do
     sleep 0.2
 done
+
 pkill vlc
