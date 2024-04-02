7ff2e209ca5ebaa370948c9ad1f57c6c470fb105
FaYviXe <54771198+FaYviXe@users.noreply.github.com>
2024-04-03 00:47:10 +0200

    Update shrek.sh

--- shrek.sh
+++ shrek.sh
@@ -1,4 +1,4 @@
-curl -o /home/$USER/Music/Shrek.mp4 "https://cdn.discordapp.com/attachments/723258310559662160/1207047699510067340/shrek.mp4?ex=661598c5&is=660323c5&hm=ee0147bdc439f426ff21215cd0841aff6ff423d9a0f0ce3f7beff1f80f0bc479&"
+#curl -o /home/$USER/Music/Shrek.mp4 "https://cdn.discordapp.com/attachments/723258310559662160/1207047699510067340/shrek.mp4?ex=661598c5&is=660323c5&hm=ee0147bdc439f426ff21215cd0841aff6ff423d9a0f0ce3f7beff1f80f0bc479&"
 
 pkill ft_lock; ft_lock
 
@@ -9,7 +9,7 @@ export LD_LIBRARY_PATH="/sgoinfre/goinfre/Perso/aboyreau/usr/lib/x86_64-linux-gn
 pactl set-sink-mute 0 false
 pactl set-sink-volume 0 +1%
 
-vlc --video-on-top --loop --no-video-deco --fullscreen --preferred-resolution 1080 ~/Music/Shrek.mp4 & 
+vlc --video-on-top --loop --no-video-deco --fullscreen --preferred-resolution 1080 /sgoinfre/goinfre/Perso/acasamit/Shrek.mp4 & 
 sleep 2
 
 window_id=`xwininfo -name "Shrek.mp4 - VLC media player" | awk '/Window id:/{print $4}'`
